951326704599971b2716b08a7ce6fff762fa26cd
balajis732005 <balajis07032005@gmail.com>
2026-01-30 21:43:12 +0530

    Inbuilt_STL_Methods

---
+++ count_method/inbuiltCount.cpp
@@ -0,0 +1,60 @@
+/***********************************************************************************************
+* STL Algorithms Demo
+* 1) count     -> Count exact value occurrences
+* 2) count_if  -> Count elements satisfying a condition
+*************************************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    vector<int> v = {1, 2, 3, 4, 3, 2, 3, 5};
+
+    /**************************************
+     * 1) count
+     **************************************/
+    cout << "===== COUNT =====" << endl;
+
+    int target = 3;
+
+    int cnt = count(v.begin(), v.end(), target);
+
+    cout << "Vector elements : ";
+    for(int x : v){
+        cout << x << " ";
+    }
+    cout << endl;
+
+    cout << "Count of " << target << " = " << cnt << endl;
+
+    cout << endl;
+
+    /**************************************
+     * 2) count_if
+     **************************************/
+    cout << "===== COUNT_IF =====" << endl;
+
+    // Count even numbers
+    int evenCount = count_if(v.begin(), v.end(),
+        [](int x){
+            return x % 2 == 0;
+        }
+    );
+
+    cout << "Even numbers count = " << evenCount << endl;
+
+    // Count numbers greater than 3
+    int greaterCount = count_if(v.begin(), v.end(),
+        [](int x){
+            return x > 3;
+        }
+    );
+
+    cout << "Numbers > 3 count = " << greaterCount << endl;
+
+    return 0;
+}
