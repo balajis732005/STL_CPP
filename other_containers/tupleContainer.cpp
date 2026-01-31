9e8718af949deaeb1c6de971b0fbe90865fcadb3
balajis732005 <balajis07032005@gmail.com>
2026-01-31 12:36:34 +0530

    Tuple_Code_Update

--- other_containers/tupleContainer.cpp
+++ other_containers/tupleContainer.cpp
@@ -4,42 +4,28 @@
 *************************************************************************************************/
 
 #include <iostream>
-#include <tuple>   // tuple
+#include <tuple>
 
 using namespace std;
 
 int main(){
 
-    // 1) Declare and initialize
     tuple<int, string, double> t1 = {1, "Balaji", 98.5};
 
-    // Access using get<index>
-    cout << get<0>(t1) << endl;
-    cout << get<1>(t1) << endl;
-    cout << get<2>(t1) << endl;
+    // ACCESS
+    cout << get<0>(t1) <<" "<< get<1>(t1) <<" "<< get<2>(t1) << endl;
 
-    cout << endl;
+    // MAKE TUPLE
+    auto t2 = make_tuple(10, "NAME", 30);
+    cout << get<0>(t2) << " "<< get<1>(t2) << " "<< get<2>(t2) << endl;
 
-    // 2) Using make_tuple
-    auto t2 = make_tuple(10, 20, 30);
-
-    cout << get<0>(t2) << " "
-         << get<1>(t2) << " "
-         << get<2>(t2) << endl;
-
-    cout << endl;
-
-    // 3) Modify values
-    get<1>(t2) = 200;
+    // MODIFY
+    get<0>(t2) = 200;
 
     cout << "Modified tuple: ";
-    cout << get<0>(t2) << " "
-         << get<1>(t2) << " "
-         << get<2>(t2) << endl;
+    cout << get<0>(t2) << " "<< get<1>(t2) << " "<< get<2>(t2) << endl;
 
-    cout << endl;
-
-    // 4) Unpacking tuple
+    // Unpacking tuple
     int a;
     string b;
     double c;
@@ -49,15 +35,11 @@ int main(){
     cout << "Unpacked values: ";
     cout << a << " " << b << " " << c << endl;
 
-    cout << endl;
-
-    // 5) Ignore some values
+    // Ignore some values in tuple
     tie(a, ignore, c) = t1;
     cout << a << " " << c << endl;
 
-    cout << endl;
-
-    // 6) Tuple comparison
+    // Tuple comparison
     tuple<int,int,int> x = {1,2,3};
     tuple<int,int,int> y = {1,2,4};
 
